596a2507d64538a4e3db3a15e9a1a4ffb575c52b
/ohnsonWTC <johnson.dubula@standardbank.co.za>
2022-11-27 16:57:49 +0200

    revision

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhoneComponent } from './phone/phone.component';
+import { PhoneListComponent } from './phone-list/phone-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneComponent,
+    PhoneListComponent
   ],
   imports: [
     BrowserModule,
